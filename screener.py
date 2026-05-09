73ddd2851137f0bd9f32ebad57f8df5b55f0395e
badrut25 <101045224+badrut25@users.noreply.github.com>
2026-05-10 00:33:09 +0700

    Update screener.py

--- screener.py
+++ screener.py
@@ -1,16 +1,86 @@
 import yfinance as yf
 import pandas as pd
 import numpy as np
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 import os
 
 # ==========================================
-# CLASS 1: KONFIGURASI PENGATURAN
+# CLASS 1: KONFIGURASI
 # ==========================================
 class Config:
-    # Masukkan ratusan daftar saham Anda di sini
-    TICKERS = ["BBCA", "BBRI", "BMRI", "BREN", "TLDN", "MTMH", "WINR", "IBOS", "OLIV", "ASHA"] # Lanjutkan daftar Anda...
+    # Masukkan daftar ratusan saham Anda di sini
+    TICKERS1 = ["BBCA", "BBRI", "BMRI", "BREN", "TLDN", "MTMH", "WINR", "IBOS", "OLIV", "ASHA"]
+    TICKERS = [
+        "AALI", "ABBA", "ABDA", "ABMM", "ACES", "ACST", "ADES", "ADHI", "ADMF", "ADMG", "ADRO", "AGII", "AGRO", "AGRS",
+        "AHAP", "AIMS", "AISA", "AKKU", "AKPI", "AKRA", "AKSI", "ALDO", "ALKA", "ALMI", "ALTO", "AMAG", "AMFG", "AMIN",
+        "AMRT", "ANJT", "ANTM", "APEX", "APIC", "APII", "APLI", "APLN", "ARGO", "ARII", "ARNA", "ARTA", "ARTI", "ARTO",
+        "ASBI", "ASDM", "ASGR", "ASII", "ASJT", "ASMI", "ASRI", "ASRM", "ASSA", "ATIC", "AUTO", "BABP", "BACA", "BAJA",
+        "BALI", "BAPA", "BATA", "BAYU", "BBCA", "BBHI", "BBKP", "BBLD", "BBMD", "BBNI", "BBRI", "BBRM", "BBTN", "BBYB",
+        "BCAP", "BCIC", "BCIP", "BDMN", "BEKS", "BEST", "BFIN", "BGTG", "BHIT", "BIKA", "BIMA", "BINA", "BIPI", "BIPP",
+        "BIRD", "BISI", "BJBR", "BJTM", "BKDP", "BKSL", "BKSW", "BLTA", "BLTZ", "BMAS", "BMRI", "BMSR", "BMTR", "BNBA",
+        "BNBR", "BNGA", "BNII", "BNLI", "BOLT", "BPFI", "BPII", "BRAM", "BRMS", "BRNA", "BRPT", "BSDE", "BSIM", "BSSR",
+        "BSWD", "BTEK", "BTEL", "BTON", "BTPN", "BUDI", "BUKK", "BULL", "BUMI", "BUVA", "BVIC", "BWPT", "BYAN", "CANI",
+        "CASS", "CEKA", "CENT", "CFIN", "CINT", "CITA", "CLPI", "CMNP", "CMPP", "CNKO", "CNTX", "COWL", "CPIN", "CPRO",
+        "CSAP", "CTBN", "CTRA", "CTTH", "DART", "DEFI", "DEWA", "DGIK", "DILD", "DKFT", "DLTA", "DMAS", "DNAR", "DNET",
+        "DOID", "DPNS", "DSFI", "DSNG", "DSSA", "DUTI", "DVLA", "DYAN", "ECII", "EKAD", "ELSA", "ELTY", "EMDE", "EMTK",
+        "ENRG", "EPMT", "ERAA", "ERTX", "ESSA", "ESTI", "ETWA", "EXCL", "FAST", "FASW", "FISH", "FMII", "FORU", "FPNI",
+        "GAMA", "GDST", "GDYR", "GEMA", "GEMS", "GGRM", "GIAA", "GJTL", "GLOB", "GMTD", "GOLD", "GOLL", "GPRA", "GSMF",
+        "GTBO", "GWSA", "GZCO", "HADE", "HDFA", "HDTX", "HERO", "HEXA", "HITS", "HMSP", "HOME", "HOTL", "HRUM", "IATA",
+        "IBFN", "IBST", "ICBP", "ICON", "IGAR", "IIKP", "IKAI", "IKBI", "IMAS", "IMJS", "IMPC", "INAF", "INAI", "INCI",
+        "INCO", "INDF", "INDR", "INDS", "INDX", "INDY", "INKP", "INPC", "INPP", "INRU", "INTA", "INTD", "INTP", "IPOL",
+        "ISAT", "ISSP", "ITMA", "ITMG", "JAWA", "JECC", "JIHD", "JKON", "JKSW", "JPFA", "JRPT", "JSMR", "JSPT", "JTPE",
+        "KAEF", "KARW", "KBLI", "KBLM", "KBLV", "KBRI", "KDSI", "KIAS", "KICI", "KIJA", "KKGI", "KLBF", "KOBX", "KOIN",
+        "KONI", "KOPI", "KPIG", "KRAH", "KRAS", "KREN", "LAPD", "LCGP", "LEAD", "LINK", "LION", "LMAS", "LMPI", "LMSH",
+        "LPCK", "LPGI", "LPIN", "LPKR", "LPLI", "LPPF", "LPPS", "LRNA", "LSIP", "LTLS", "MAGP", "MAIN", "MAMI", "MAPI",
+        "MAYA", "MBAP", "MBSS", "MBTO", "MCOR", "MDIA", "MDKA", "MDLN", "MDRN", "MEDC", "MEGA", "MERK", "META",
+        "MFMI", "MGNA", "MICE", "MIDI", "MIKA", "MIRA", "MITI", "MKPI", "MLBI", "MLIA", "MLPL", "MLPT", "MMLP",
+        "MNCN", "MPMX", "MPPA", "MRAT", "MREI", "MSKY", "MTDL", "MTFN", "MTLA", "MTSM", "MYOH", "MYOR", "MYRX", "MYTX",
+        "NELY", "NIKL", "NIPS", "NIRO", "NISP", "NOBU", "NRCA", "OCAP", "OKAS", "OMRE", "PADI", "PALM", "PANR", "PANS",
+        "PBRX", "PDES", "PEGE", "PGAS", "PGLI", "PICO", "PJAA", "PKPK", "PLAS", "PLIN", "PNBN", "PNBS", "PNIN", "PNLF",
+        "PNSE", "POLY", "POOL", "PPRO", "PRAS", "PSAB", "PSDN", "PSKT", "PTBA", "PTIS", "PTPP", "PTRO", "PTSN", "PTSP",
+        "PUDP", "PWON", "PYFA", "RAJA", "RALS", "RANC", "RBMS", "RDTX", "RELI", "RICY", "RIGS", "RIMO", "RODA", "ROTI",
+        "RUIS", "SAFE", "SAME", "SCCO", "SCMA", "SCPI", "SDMU", "SDPC", "SDRA", "SGRO", "SHID", "SIDO", "SILO", "SIMA",
+        "SIMP", "SIPD", "SKBM", "SKLT", "SKYB", "SMAR", "SMBR", "SMCB", "SMDM", "SMDR", "SMGR", "SMMA", "SMMT", "SMRA",
+        "SMRU", "SMSM", "SOCI", "SONA", "SPMA", "SQMI", "SRAJ", "SRIL", "SRSN", "SRTG", "SSIA", "SSMS", "SSTM", "STAR",
+        "STTP", "SUGI", "SULI", "SUPR", "TALF", "TARA", "TAXI", "TBIG", "TBLA", "TBMS", "TCID", "TELE", "TFCO", "TGKA",
+        "TIFA", "TINS", "TIRA", "TIRT", "TKIM", "TLKM", "TMAS", "TMPO", "TOBA", "TOTL", "TOTO", "TOWR", "TPIA", "TPMA",
+        "TRAM", "TRIL", "TRIM", "TRIO", "TRIS", "TRST", "TRUS", "TSPC", "ULTJ", "UNIC", "UNIT", "UNSP", "UNTR", "UNVR",
+        "VICO", "VINS", "VIVA", "VOKS", "VRNA", "WAPO", "WEHA", "WICO", "WIIM", "WIKA", "WINS", "WOMF", "WSKT", "WTON",
+        "YPAS", "YULE", "ZBRA", "SHIP", "CASA", "DAYA", "DPUM", "IDPR", "JGLE", "KINO", "MARI", "MKNT", "MTRA", "OASA",
+        "POWR", "INCF", "WSBP", "PBSA", "PRDA", "BOGA", "BRIS", "PORT", "CARS", "MINA", "FORZ", "CLEO", "TAMU", "CSIS",
+        "TGRA", "FIRE", "TOPS", "KMTR", "ARMY", "MAPB", "WOOD", "HRTA", "MABA", "HOKI", "MPOW", "MARK", "NASA", "MDKI",
+        "BELL", "KIOS", "GMFI", "MTWI", "ZINC", "MCAS", "PPRE", "WEGE", "PSSI", "MORA", "DWGL", "PBID", "JMAS", "CAMP",
+        "IPCM", "PCAR", "LCKM", "BOSS", "HELI", "JSKY", "INPS", "GHON", "TDPM", "DFAM", "NICK", "BTPS", "SPTO", "PRIM",
+        "HEAL", "TRUK", "PZZA", "TUGU", "MSIN", "SWAT", "KPAL", "TNCA", "MAPA", "TCPI", "IPCC", "RISE", "BPTR", "POLL",
+        "NFCX", "MGRO", "NUSA", "FILM", "ANDI", "LAND", "MOLI", "PANI", "DIGI", "CITY", "SAPX", "KPAS", "SURE", "HKMU",
+        "MPRO", "DUCK", "GOOD", "SKRN", "YELO", "CAKK", "SATU", "SOSS", "DEAL", "POLA", "DIVA", "LUCK", "URBN", "SOTS",
+        "ZONE", "PEHA", "FOOD", "BEEF", "POLI", "CLAY", "NATO", "JAYA", "COCO", "MTPS", "CPRI", "HRME", "POSA", "JAST",
+        "FITT", "BOLA", "CCSI", "SFAN", "POLU", "KJEN", "KAYU", "ITIC", "PAMG", "IPTV", "BLUE", "ENVY", "EAST", "LIFE",
+        "FUJI", "KOTA", "INOV", "ARKA", "SMKL", "HDIT", "KEEN", "BAPI", "TFAS", "GGRP", "OPMS", "NZIA", "SLIS", "PURE",
+        "IRRA", "DMMX", "SINI", "WOWS", "ESIP", "TEBE", "KEJU", "PSGO", "AGAR", "IFSH", "REAL", "IFII", "PMJS", "UCID",
+        "GLVA", "PGJO", "AMAR", "CSRA", "INDO", "AMOR", "TRIN", "DMND", "PURA", "PTPW", "TAMA", "IKAN", "AYLS", "DADA",
+        "ASPI", "ESTA", "BESS", "AMAN", "CARE", "SAMF", "SBAT", "KBAG", "CBMF", "RONY", "CSMI", "BBSS", "BHAT", "CASH",
+        "TECH", "EPAC", "UANG", "PGUN", "SOFA", "PPGL", "TOYS", "SGER", "TRJA", "PNGO", "SCNP", "BBSI", "KMDS", "PURI",
+        "SOHO", "HOMI", "ROCK", "ENZO", "PLAN", "PTDU", "ATAP", "VICI", "PMMP", "WIFI", "FAPA", "DCII", "KETR", "DGNS",
+        "UFOE", "BANK", "WMUU", "EDGE", "UNIQ", "BEBS", "SNLK", "ZYRX", "LFLO", "FIMP", "TAPG", "NPGF", "LUCY", "ADCP",
+        "HOPE", "MGLV", "TRUE", "LABA", "ARCI", "IPAC", "MASB", "BMHS", "FLMC", "NICL", "UVCR", "BUKA", "HAIS", "OILS",
+        "GPSO", "MCOL", "RSGK", "RUNS", "SBMA", "CMNT", "GTSI", "IDEA", "KUAS", "BOBA", "MTEL", "DEPO", "BINO", "CMRY",
+        "WGSH", "TAYS", "WMPP", "RMKE", "OBMD", "AVIA", "IPPE", "NASI", "BSML", "DRMA", "ADMR", "SEMA", "ASLC", "NETV",
+        "BAUT", "ENAK", "NTBK", "SMKM", "STAA", "NANO", "BIKE", "WIRG", "SICO", "GOTO", "TLDN", "MTMH", "WINR", "IBOS",
+        "OLIV", "ASHA", "SWID", "TRGU", "ARKO", "CHEM", "DEWI", "AXIO", "KRYA", "HATM", "RCCC", "GULA", "JARR", "AMMS",
+        "RAFI", "KKES", "ELPI", "EURO", "KLIN", "TOOL", "BUAH", "CRAB", "MEDS", "COAL", "PRAY", "CBUT", "BELI", "MKTR",
+        "OMED", "BSBK", "PDPP", "KDTN", "ZATA", "NINE", "MMIX", "PADA", "ISAP", "VTNY", "SOUL", "ELIT", "BEER", "CBPE",
+        "SUNI", "CBRE", "WINE", "BMBL", "PEVE", "LAJU", "FWCT", "NAYZ", "IRSX", "PACK", "VAST", "CHIP", "HALO", "KING",
+        "PGEO", "FUTR", "HILL", "BDKR", "PTMP", "SAGE", "TRON", "CUAN", "NSSS", "GTRA", "HAJJ", "PIPA", "NCKL", "MENN",
+        "AWAN", "MBMA", "RAAM", "DOOH", "JATI", "TYRE", "MPXL", "SMIL", "KLAS", "MAXI", "VKTR", "RELF", "AMMN", "CRSN",
+        "GRPM", "WIDI", "TGUK", "INET", "MAHA", "RMKO", "CNMA", "FOLK", "HBAT", "GRIA", "PPRI", "ERAL", "CYBR", "MUTU",
+        "LMAX", "HUMI", "MSIE", "RSCH", "BABY", "AEGS", "IOTF", "KOCI", "PTPS", "BREN", "STRK", "KOKA", "LOPI", "UDNG",
+        "RGAS", "MSTI", "IKPM", "AYAM", "SURI", "ASLI", "CGAS", "NICE", "MSJA", "SMLE", "ACRO", "MANG", "GRPH", "SMGA",
+        "UNTD", "TOSK", "MPIX", "ALII", "MKAP", "MEJA", "LIVE", "HYGN", "BAIK", "VISI", "AREA", "MHKI", "ATLA", "DATA",
+        "SOLA", "BATR", "SPRE", "PART", "GOLF", "ISEA", "BLES", "GUNA", "LABS", "DOSS", "NEST", "PTMR", "VERN", "DAAZ",
+        "BOAT", "NAIK", "AADI", "MDIY", "KSIX", "RATU", "YOII", "HGII", "BRRC", "DGWG", "CBDK", "OBAT", "MINE", "KAQI",
+        "YUPI", "FORE", "MDLA", "DKHH", "PSAT", "CDIA", "COIN", "BLOG", "CHEK", "MERI", "ASPR", "PMUI", "EMAS", "PJHB",
+        "RLCO", "SUPA"
+    ]
     PERIOD = "1y"
     TEMPLATE_FILE = "template.html"
     OUTPUT_FILE = "index.html"
@@ -23,7 +93,6 @@ class Indicators:
     def calc_hhma(src_series, length=24, tension=2.0):
         halfLen = max(1, int(np.floor(length / 2)))
         sqrtLen = max(1, int(np.floor(np.sqrt(length))))
-        
         def f_sinh_weight(series, l, t):
             weights = []
             for i in range(l):
@@ -44,7 +113,7 @@ class Indicators:
         return src_series.ewm(span=length, adjust=False).mean()
 
 # ==========================================
-# CLASS 3: ANALISATOR SAHAM (PROSES 1 SAHAM)
+# CLASS 3: ANALISATOR SAHAM
 # ==========================================
 class StockAnalyzer:
     def __init__(self, ticker):
@@ -54,8 +123,7 @@ class StockAnalyzer:
 
     def fetch_data(self):
         df = yf.download(self.yf_ticker, period=Config.PERIOD, progress=False)
-        if df.empty:
-            return False
+        if df.empty: return False
         if isinstance(df.columns, pd.MultiIndex):
             df.columns = df.columns.get_level_values(0)
         self.df = df
@@ -65,6 +133,7 @@ class StockAnalyzer:
         self.df['HHMA'] = Indicators.calc_hhma(self.df['Close'])
         self.df['EMA9'] = Indicators.calc_ema(self.df['Close'], 9)
         self.df['EMA21'] = Indicators.calc_ema(self.df['Close'], 21)
+        self.df['SMA20_Vol'] = self.df['Volume'].rolling(20).mean()
         
         self.df['isBullish'] = self.df['HHMA'] > self.df['HHMA'].shift(1)
         self.df['turned_bullish'] = self.df['isBullish'] & (~self.df['isBullish'].shift(1).fillna(False))
@@ -74,89 +143,128 @@ class StockAnalyzer:
         self.df['cond_sell'] = self.df['turned_bearish']
 
     def has_signal_today(self):
-        """Mengecek apakah hari terakhir terdapat sinyal BUY atau SELL"""
-        if self.df.empty:
-            return False
-        last_row = self.df.iloc[-1]
-        return bool(last_row['cond_buy'] or last_row['cond_sell'])
+        if self.df.empty: return False
+        return bool(self.df.iloc[-1]['cond_buy'] or self.df.iloc[-1]['cond_sell'])
 
-    def generate_chart_html(self):
-        df = self.df
-        fig = make_subplots(rows=2, cols=1, shared_xaxes=True, row_heights=[0.8, 0.2])
-        
-        fig.add_trace(go.Candlestick(x=df.index, open=df['Open'], high=df['High'], low=df['Low'], close=df['Close'], name="Price"), row=1, col=1)
-        fig.add_trace(go.Scatter(x=df.index, y=df['HHMA'], line=dict(color='blue', width=2), name="HHMA"), row=1, col=1)
-        fig.add_trace(go.Scatter(x=df.index, y=df['EMA9'], line=dict(color='yellow', width=1), name="EMA 9"), row=1, col=1)
-        fig.add_trace(go.Scatter(x=df.index, y=df['EMA21'], line=dict(color='orange', width=1), name="EMA 21"), row=1, col=1)
-        
-        buy_data = df[df['cond_buy']]
-        sell_data = df[df['cond_sell']]
-        
-        fig.add_trace(go.Scatter(x=buy_data.index, y=buy_data['Low'] * 0.98, mode='markers+text', marker=dict(symbol='triangle-up', size=12, color='green'), text=["BUY"]*len(buy_data), textposition="bottom center", name="BUY"), row=1, col=1)
-        fig.add_trace(go.Scatter(x=sell_data.index, y=sell_data['High'] * 1.02, mode='markers+text', marker=dict(symbol='triangle-down', size=12, color='red'), text=["SELL"]*len(sell_data), textposition="top center", name="SELL"), row=1, col=1)
+    def get_fundamentals(self):
+        """Menarik data PER & PBV dari Yahoo Finance"""
+        try:
+            info = yf.Ticker(self.yf_ticker).info
+            per = info.get('trailingPE', 'N/A')
+            pbv = info.get('priceToBook', 'N/A')
+            
+            per_str = f"{per:.2f}" if isinstance(per, (int, float)) else "N/A"
+            pbv_str = f"{pbv:.2f}" if isinstance(pbv, (int, float)) else "N/A"
+            return per_str, pbv_str
+        except:
+            return "N/A", "N/A"
 
-        fig.add_trace(go.Bar(x=df.index, y=df['Volume'], name="Volume", marker_color='gray'), row=2, col=1)
+    def generate_table_row(self):
+        """Menghasilkan 1 baris HTML (<tr>) untuk tabel"""
+        last = self.df.iloc[-1]
+        prev = self.df.iloc[-2]
         
-        # Tambahkan status ke Judul
-        status = "🟢 BUY SIGNAL" if df.iloc[-1]['cond_buy'] else "🔴 SELL SIGNAL"
-        fig.update_layout(title=f"Chart Saham: {self.ticker} [{status}]", xaxis_rangeslider_visible=False, template="plotly_dark", height=600)
-        return fig.to_html(full_html=False, include_plotlyjs='cdn')
+        # Format Data
+        tanggal = self.df.index[-1].strftime('%Y-%m-%d')
+        pola = "<span class='buy-text'>BUY 🚀</span>" if last['cond_buy'] else "<span class='sell-text'>SELL ⚠️</span>"
+        close = f"{last['Close']:,.0f}"
+        
+        pct_change = ((last['Close'] - prev['Close']) / prev['Close']) * 100
+        pct_color = "#00ffaa" if pct_change > 0 else "#ff4444"
+        pct_str = f"<span style='color: {pct_color};'>{pct_change:.2f}%</span>"
+        
+        # Format Volume
+        vol = last['Volume']
+        if vol >= 1e9: vol_str = f"{vol/1e9:.2f}B"
+        elif vol >= 1e6: vol_str = f"{vol/1e6:.2f}M"
+        elif vol >= 1e3: vol_str = f"{vol/1e3:.2f}K"
+        else: vol_str = str(vol)
+        
+        spike_vol = "Ya 🔥" if vol > (last['SMA20_Vol'] * 1.5) else "Tidak"
+        warna_kernel = "Hijau 🟢" if last['isBullish'] else "Merah 🔴"
+        trend = "Uptrend 📈" if last['EMA9'] > last['EMA21'] else "Downtrend 📉"
+        
+        # Tarik Fundamental HANYA jika ada sinyal (agar cepat)
+        per, pbv = self.get_fundamentals()
+        
+        row_html = f"""
+        <tr>
+            <td style='font-weight:bold;'>{self.ticker}</td>
+            <td>{tanggal}</td>
+            <td>{pola}</td>
+            <td>{close}</td>
+            <td>{pct_str}</td>
+            <td>{vol_str}</td>
+            <td>{spike_vol}</td>
+            <td>{warna_kernel}</td>
+            <td>{per}</td>
+            <td>{pbv}</td>
+            <td>{trend}</td>
+        </tr>
+        """
+        return row_html
 
 # ==========================================
-# CLASS 4: MAIN APP (PENGENDALI UTAMA)
+# CLASS 4: MAIN APP
 # ==========================================
 class ScreenerApp:
     def __init__(self):
-        self.charts_html = ""
-        self.stocks_with_signals = 0 # Menghitung berapa saham yang punya sinyal
+        # Header Tabel
+        self.table_html = """
+        <table>
+            <thead>
+                <tr>
+                    <th>Ticker</th>
+                    <th>Tanggal</th>
+                    <th>Pola</th>
+                    <th>Harga Close</th>
+                    <th>% Ubah</th>
+                    <th>Volume</th>
+                    <th>Spike Vol?</th>
+                    <th>Warna Kernel</th>
+                    <th>PER</th>
+                    <th>PBV</th>
+                    <th>Trend</th>
+                </tr>
+            </thead>
+            <tbody>
+        """
+        self.signals_count = 0
 
     def run(self):
-        print("Mulai menganalisa saham...")
+        print("Mulai Screening...")
         for ticker in Config.TICKERS:
             try:
                 analyzer = StockAnalyzer(ticker)
-                if not analyzer.fetch_data():
-                    print(f"   [-] Data {ticker} tidak ditemukan.")
-                    continue
-                    
+                if not analyzer.fetch_data(): continue
                 analyzer.calculate_signals()
                 
-                # FITUR BARU: Hanya buat chart jika ada sinyal hari ini!
                 if analyzer.has_signal_today():
-                    print(f"-> 🎯 SINYAL DITEMUKAN pada {ticker}! Sedang menggambar chart...")
-                    chart_div = analyzer.generate_chart_html()
-                    self.charts_html += f"<div class='chart-box'>{chart_div}</div>\n"
-                    self.stocks_with_signals += 1
-                else:
-                    # Jangan digambar untuk menghemat memori
-                    print(f"-> Melewati {ticker} (Tidak ada sinyal)")
-                
+                    print(f"🎯 Sinyal Ditemukan: {ticker}")
+                    self.table_html += analyzer.generate_table_row()
+                    self.signals_count += 1
             except Exception as e:
-                print(f"   [!] Error pada {ticker}: {e}")
+                print(f"[!] Error {ticker}: {e}")
 
-        # Jika tidak ada satu pun sinyal hari ini
-        if self.stocks_with_signals == 0:
-            self.charts_html = "<h2 style='text-align:center; color:gray;'>Tidak ada sinyal Buy/Sell hari ini.</h2>"
-
+        # Tutup Tabel
+        self.table_html += "</tbody></table>"
+        
+        if self.signals_count == 0:
+            self.table_html = "<h3 style='text-align:center; color:#888;'>Tidak ada sinyal BUY/SELL di hari bursa terakhir.</h3>"
+            
         self.inject_to_html()
 
     def inject_to_html(self):
-        if not os.path.exists(Config.TEMPLATE_FILE):
-            print(f"CRITICAL ERROR: File {Config.TEMPLATE_FILE} tidak ditemukan!")
-            return
+        if not os.path.exists(Config.TEMPLATE_FILE): return
+        with open(Config.TEMPLATE_FILE, "r", encoding="utf-8") as f:
+            template = f.read()
 
-        with open(Config.TEMPLATE_FILE, "r", encoding="utf-8") as file:
-            template_content = file.read()
+        final_html = template.replace("<!-- TABLE_GOES_HERE -->", self.table_html)
 
-        # PERBAIKAN BUG REPLACE: Memastikan kata kunci yang diganti itu akurat
-        final_html = template_content.replace("", self.charts_html)
+        with open(Config.OUTPUT_FILE, "w", encoding="utf-8") as f:
+            f.write(final_html)
+        print(f"SELESAI! Ditemukan {self.signals_count} saham.")
 
-        with open(Config.OUTPUT_FILE, "w", encoding="utf-8") as file:
-            file.write(final_html)
-            
-        print(f"SELESAI! Ditemukan {self.stocks_with_signals} saham dengan sinyal hari ini.")
-
-# Eksekusi Program Utama
 if __name__ == "__main__":
     app = ScreenerApp()
     app.run()
